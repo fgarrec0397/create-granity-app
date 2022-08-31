5aed543273a4c2931207a588227d71ea18733029
fga97 <fgarrec0397@gmail.com>
2022-08-31 19:44:51 -0400

    Refactoring command line + test delete file

---
+++ bin/index.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const fs = require('fs');
+
+const displayMessage = (text) => {
+    console.log();
+    console.log(text);
+    console.log();
+}
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, { stdio: "inherit" });
+    } catch (error) {
+        displayMessage(`Failed to execute ${command}`, error);
+        
+        return false;
+    }
+    
+    return true;
+};
+
+const deletefile = (file) => {
+    fs.unlink(file, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log("File is deleted.");
+    });
+};
+
+const exit = (withError) => {
+    displayMessage("Process has terminated.")
+    process.exit(withError ? 1 : 0);    
+};
+
+const gitRepo = "https://github.com/fgarrec0397/Granity.git";
+const projectName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 ${gitRepo} ${projectName}`;
+const installDepsCommand = `cd ${projectName} && npm install && cd app && npm install && cd ../server && npm install`
+
+const cloneRepository = () => {
+    displayMessage(`Cloning the repositery with name ${projectName}`);
+    
+    const checkedOut = runCommand(gitCheckoutCommand);
+    
+    if (!checkedOut) {
+        exit(true);
+    }
+}
+
+const cleanUpRepo = () => {
+    displayMessage("Hold on! Just removing our crap for you...");
+
+    // delete a file
+    deletefile("CODE_OF_CONDUCT.md")
+    
+}
+
+const installDependencies = () => {
+    console.log(`Installing dependencies for ${projectName}`);
+    
+    const installingCommand = runCommand(installDepsCommand);
+    
+    if (!installingCommand) {
+        exit(true);
+    }
+};
+
+const init = () => {
+    cloneRepository();
+    cleanUpRepo();
+    installDependencies();
+};
+
+
+console.log(`Congratulation! You are now ready to work`);
+
+init();
